d02e00742cee5f379d96bfd7ba519b38ccdc68a9
giacomo <giacomo10.rossi@mail.polimi.it>
2023-04-11 22:19:01 +0200

    color the buttons
    and make the code prettier

--- index.js
+++ index.js
@@ -1,29 +1,67 @@
 function App() {
-    return (
-        <div className="container">
-            <div className="grid">
-                <div className="dis"></div>
-                <div className="padButton AC">AC</div>
-                <div className="padButton C">C</div>
-                <div className="padButton div">/</div>
-                <div className="padButton times">x</div>
-                <div className="padButton seven">7</div>
-                <div className="padButton eight">8</div>
-                <div className="padButton nine">9</div>
-                <div className="padButton minus">-</div>
-                <div className="padButton four">4</div>
-                <div className="padButton five">5</div>
-                <div className="padButton six">6</div>
-                <div className="padButton plus">+</div>
-                <div className="padButton one">1</div>
-                <div className="padButton two">2</div>
-                <div className="padButton three">3</div>
-                <div className="padButton equal">=</div>
-                <div className="padButton zero">0</div>
-                <div className="padButton dot">.</div>
-            </div>
-        </div> 
-    )
+  const display = () => {};
+
+  return (
+    <div className="container">
+      <div className="grid">
+        <div onClick={display} className="dis"></div>
+        <div onClick={display} className="padButton AC tomato">
+          AC
+        </div>
+        <div onClick={display} className="padButton C tomato">
+          C
+        </div>
+        <div onClick={display} className="padButton div">
+          /
+        </div>
+        <div onClick={display} className="padButton times">
+          x
+        </div>
+        <div onClick={display} className="padButton seven dark-grey">
+          7
+        </div>
+        <div onClick={display} className="padButton eight dark-grey">
+          8
+        </div>
+        <div onClick={display} className="padButton nine dark-grey">
+          9
+        </div>
+        <div onClick={display} className="padButton minus">
+          -
+        </div>
+        <div onClick={display} className="padButton four dark-grey">
+          4
+        </div>
+        <div onClick={display} className="padButton five dark-grey">
+          5
+        </div>
+        <div onClick={display} className="padButton six dark-grey">
+          6
+        </div>
+        <div onClick={display} className="padButton plus">
+          +
+        </div>
+        <div onClick={display} className="padButton one dark-grey">
+          1
+        </div>
+        <div onClick={display} className="padButton two dark-grey">
+          2
+        </div>
+        <div onClick={display} className="padButton three dark-grey">
+          3
+        </div>
+        <div onClick={display} className="padButton equal blue">
+          =
+        </div>
+        <div onClick={display} className="padButton zero dark-grey">
+          0
+        </div>
+        <div onClick={display} className="padButton dot dark-grey">
+          .
+        </div>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
